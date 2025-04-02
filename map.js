a1b4972ce6bfbd901e72ad4ed0eccef0d7d44e04
Eugene Kalinoŭski <yauheni.kalinouski@pandadoc.com>
2025-04-02 20:45:48 +0200

    Apr 2: Added map animation steps

--- map.js
+++ map.js
@@ -1,14 +1,55 @@
 const styleUrl = "mapbox://styles/nk-studio/cm8rqp5e900cx01s77ju3fbhg/draft";
 mapboxgl.accessToken = TOKEN;
-BIRDS_FLYWAYS_LAYER_ID = "BIRDS_FLYWAYS";
+BIRDS_FLYWAYS_LAYER_ID = "BIRDS_FLYWAYS - BASE";
 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: styleUrl, // style URL
-    center: [30, 30], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: styleUrl,
+    center: [32, 25],
+    zoom: 1.6,
 });
+
+// Global Urban Footprint layer
 GUF_LAYER_NAME = 'GUF28_DLR_v1_Mosaic';
+
+var scheduledAnimationStep = null;
+async function animateMap(views, step=1) {
+    if (scheduledAnimationStep) {
+        clearTimeout(scheduledAnimationStep);
+        console.log(`Timeout ${scheduledAnimationStep} cleared`)
+        scheduledAnimationStep = null;
+    }
+    const [z, lon, lat, speed, waitUntilNext, onMoveend] = views.shift();
+    console.log(`Step ${step} started`);
+
+    map.flyTo({
+        center: [lat, lon], // Moves to Los Angeles
+        zoom: z,
+        speed: speed, // Slow down the movement
+        curve: 1.1,
+    });
+
+    map.once('moveend', () => {
+        console.log("Moveend triggered");
+        onMoveend ? onMoveend() : null;
+        console.log(`Step ${step} completed`);
+        if (views.length > 0) {
+            scheduledAnimationStep = setTimeout(async () => {
+                    await animateMap(views, step + 1);
+                },
+                waitUntilNext * 1000
+            );
+        } else {
+            console.info("All steps completed.");
+        }
+    });
+
+}
+
+function runAnimation(views) {
+    animateMap(views.slice());
+}
+
 
 map.on('load', () => {
     map.addSource('wms-source', {
@@ -19,15 +60,51 @@ map.on('load', () => {
         tileSize: 256
     });
     map.addLayer({
-        id: 'guf-layer',
-        type: 'raster',
-        source: 'wms-source',
-        paint: {
-            "raster-opacity": 0.8,
-            'raster-saturation': 0.8,
-            'raster-fade-duration': 500,
+            id: 'guf-layer',
+            type: 'raster',
+            source: 'wms-source',
+            paint: {
+                "raster-opacity": 0.8,
+                'raster-saturation': 0.8,
+                'raster-fade-duration': 500,
+            },
         },
-    },
-        BIRDS_FLYWAYS_LAYER_ID  // insert before this layer
-        );
+        BIRDS_FLYWAYS_LAYER_ID // insert before this layer
+    );
+
 });
+
+
+// Animation part
+
+const animationFlowViews = [
+    // zoom, lon, lat, speed, waitUntilNext, onComplete
+    [
+        3.07, 24.7, 30.33,
+        0.12, 0, null
+    ],
+    [
+        5.4, 26.667, 31.295,
+        0.12, 0, null
+    ],
+    [
+        6.12, 28.752, 30.16,
+        0.12, 0, null
+    ],
+    [
+        7.79, 30.037, 30.666,
+        0.12, 0, () => {
+            console.log("Callback completed!")
+        }
+    ],
+    [
+        8.84, 30.2206, 29.9583,
+        0.12, 0, null
+    ],
+    [
+        13.08, 30.43664, 29.53953,
+        0.12, 0, null
+    ],
+]
+
+runAnimation(animationFlowViews);
